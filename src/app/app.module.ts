707e95a9e2c3d9a60b743628a6f20304531c9091
Pablo Roberto Carrera Estrada <pablocarrera3@gmail.com>
2017-06-01 16:48:24 -0500

    adding rollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,28 @@
+import Rollbar = require('rollbar');
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable, Injector } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+const rollbarConfig = {
+  accessToken: 'c06b4919cc0d49ee813c235131c50cb5',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'development'
+  }
+};
+
+@Injectable()
+export class RollbarErrorHandler implements ErrorHandler {
+  constructor(private injector: Injector) { }
+  handleError(err: any): void {
+    const rollbar = this.injector.get(Rollbar);
+    rollbar.error(err.originalError || err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +33,13 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: RollbarErrorHandler },
+    { provide: Rollbar,
+      useFactory: () => {
+        return new Rollbar(rollbarConfig);
+      }
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
