acf670a70119f971b13e90458e678f598e8f1a1d
Pablo Roberto Carrera Estrada <pablocarrera3@gmail.com>
2017-07-06 11:01:21 -0500

    working on header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { RollbarErrorHandler, useFactory } from './rollbar/rollbar.error.handler';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 
@@ -19,8 +18,6 @@ import { HeaderModule } from './header/header.module';
     HeaderModule
   ],
   providers: [
-    { provide: ErrorHandler, useClass: RollbarErrorHandler },
-    { provide: Rollbar, useFactory },
   ],
   bootstrap: [AppComponent]
 })
